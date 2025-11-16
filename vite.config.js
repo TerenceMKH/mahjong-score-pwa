aaf3ad45edf1ce194f8d86792b55c4e7d7e6047a
TerenceMKH <terencemkh@gmail.com>
2025-11-16 11:01:51 +0800

    Fix gitignore and add base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/mahjong-score-pwa/', // Add this line
   plugins: [
     react(),
     VitePWA({
